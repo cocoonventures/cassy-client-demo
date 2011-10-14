d05644309c4c8a259cf029daa9971faf123984f7
Mac Martine <public@macmartine.com>
2011-10-13 22:40:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CassyClientDemo::Application.config.secret_token = '303e4c1348fcb978e73dd7d53f25e5d7905b808e67e9016af0cc61a03f93bb02bb01d56964ba8a448f92790b4ec592f61498e6deebbb964dc3a99795a7ebf5de'
